bf147bca8d6d64fca577b592b73d9e2157211f69
erkileemet <erki@K2-01.ametikool.local>
2020-03-10 11:49:23 +0200

    stuff

---
+++ app.js
@@ -0,0 +1,36 @@
+let occupations =  {
+    3514: {name:'Arvutivõrkude tippspetsialist', rate: 0},
+    2523: {name:'Veebitehnik', rate: 0},
+    5120: {name:'Kokad', rate: 0},
+    5245: {name:'Tanklatöötaja', rate: 0},
+    5412: {name:'Politseinik', rate: 0}
+}
+
+fetch("http://andmebaas.stat.ee/sdmx-json/data/PA633/DBL103+83+DBL245+DBL269+DBL278+DBL288.3.1/all?startTime=2014&endTime=2014&dimensionAtObservation=allDimensions")
+.then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data)
+    Object.keys(data.dataSets[0].observations).forEach(key => {
+        console.log(data.dataSets[0].observations[key][0])
+        const key_value = data.dataSets[0].observations[key][0]
+        let elementDiv = document.createElement("div")
+        elementDiv.innerHTML = "<div>" + key_value + "</div>"
+        elementDiv.classList.add("test")
+        value.append(elementDiv)
+
+        
+    });
+
+    data.structure.dimensions.observation[0].values.forEach((element, i) => {
+        const occupationKey = element.name.split(' ')[0]
+        const dataSetsKey = i + ':0:0:0'
+        const dataSetsKeyValues = data.dataSets[0].observations[dataSetsKey][0]
+        occupations[occupationKey].rate = dataSetsKeyValues
+        console.log(occupationKey)
+        console.log(dataSetsKey)
+        console.log(dataSetsKeyValues)
+        
+    });
+    console.log(occupations)
+})
